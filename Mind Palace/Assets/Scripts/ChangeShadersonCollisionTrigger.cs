84b4d086aa3197d56decb084baa49a86d7a843b5
Viswadeep Sarangi <vs743@its.york.ac.uk>
2020-08-07 23:10:15 +0100

    made my first shader

---
+++ Mind Palace/Assets/Scripts/ChangeShadersonCollisionTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ChangeShadersonCollisionTrigger : MonoBehaviour
+{
+    [SerializeField] Shader normalShader;
+    [SerializeField] Shader highlightedShader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<MeshRenderer>().material.shader = normalShader;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+        GetComponent<MeshRenderer>().material.shader = highlightedShader;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit");
+        GetComponent<MeshRenderer>().material.shader = normalShader;
+    }
+
+
+}
